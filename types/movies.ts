da5a095a9ed44c5b238596c14761c82c90285040
Cameron Howson <cameron.howson@sidigital.co>
2023-07-19 11:02:37 +0100

    add movie types

--- types/movies.ts
+++ types/movies.ts
@@ -29,3 +29,31 @@ export interface RequestBodyIdPageSort extends NextApiRequest {
       | 'vote_count.desc';
   };
 }
+
+export interface Movie {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: MovieItem[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieItem {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
