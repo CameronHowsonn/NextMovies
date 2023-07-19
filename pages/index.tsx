d97b16f4a45b15a1d1ad8a8e49f485bc5c49a2e3
Cameron Howson <cameron.howson@sidigital.co>
2023-07-19 11:05:23 +0100

    fetch upcoming films for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BrowseHero from '../components/browse/hero';
+import { MovieItem } from '../types/movies';
+import FullWidthSlider from '../components/full-width-slider';
 
 const Index: React.FC = () => {
+  const [upcomingFilms, setUpcomingFilms] = useState<MovieItem[]>(null);
+
+  useEffect(() => {
+    fetch('/api/movies/get-upcoming', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ page: '1' }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results.length);
+        setUpcomingFilms(data.results);
+      });
+  }, []);
+
   return (
     <>
       <BrowseHero />
+      <FullWidthSlider
+        data={upcomingFilms}
+        title='Upcoming Films'
+        slidesPerView={6}
+      />
     </>
   );
 };
