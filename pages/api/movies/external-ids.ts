345e1b4fe486f5e787dec9792a54ee76ff2e8ef0
Cameron Howson <cameron.howson@sidigital.co>
2023-07-25 16:03:46 +0100

    add movie external ids api end point

---
+++ pages/api/movies/external-ids.ts
@@ -0,0 +1,21 @@
+import { NextApiResponse } from 'next';
+import { RequestBodyId } from '../../../types/movies';
+
+const ExternalIds = async (req: RequestBodyId, res: NextApiResponse) => {
+  const { id } = req.body;
+
+  if (!id) return res.status(400).json({ error: 'Missing id' });
+
+  await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/external_ids?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+export default ExternalIds;
