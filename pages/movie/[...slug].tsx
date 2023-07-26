91955d49dcc0b4a960c3b48c926bc8b17f5b6289
Cameron Howson <cameron.howson@sidigital.co>
2023-07-26 15:47:50 +0100

    fetch reviews and add reviews component

--- pages/movie/[...slug].tsx
+++ pages/movie/[...slug].tsx
@@ -5,12 +5,14 @@ import styled from 'styled-components';
 import FullWidthSlider from '../../components/full-width-slider';
 import MovieHero from '../../components/movie-hero';
 import MovieStats from '../../components/movie-stats';
-import { MovieItem } from '../../types/movies';
+import Reviews from '../../components/reviews';
+import { MovieItem, MovieReviews, Review } from '../../types/movies';
 
 const MovieSlug = () => {
   const router = useRouter();
   const [filmData, setFilmData] = useState<MovieItem>(null);
   const [credits, setCredits] = useState(null);
+  const [reviews, setReviews] = useState<Review[]>(null);
   const [error, setError] = useState(false);
   const [filmId, setFilmId] = useState(null);
   const [similar, getSimilar] = useState(null);
@@ -90,6 +92,26 @@ const MovieSlug = () => {
           getImages(data);
           console.log(data);
         });
+
+      // api/movies/get-reviews
+      await fetch(`/api/movies/get-reviews`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: filmId,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data: MovieReviews) => {
+          console.log(data.results);
+          setReviews(data.results);
+        })
+        .catch((err) => {
+          setError(true);
+          toast.error('Cannot fetch movie data.');
+        });
     };
     if (filmId) {
       getData();
@@ -112,6 +134,7 @@ const MovieSlug = () => {
         )}
         {filmData && <MovieStats {...filmData} />}
       </MovieContainer>
+      {reviews && <Reviews reviews={reviews} />}
       {similar && (
         <FullWidthSlider
           data={similar}
